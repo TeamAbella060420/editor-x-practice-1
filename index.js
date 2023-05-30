5a5eadd7b6f729a70a5d99be6b3b8746272a5c19
TeamAbella060420 <teamabella@outlook.com>
2023-05-30 13:00:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // Get the hamburger button and mobile menu elements
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
+const closeButton = document.querySelector('.close');
 
 // Add a click event listener to the hamburger button
 hamburger.addEventListener('click', toggleMobileMenu);
 
+// Add a click event listener to the close button
+closeButton.addEventListener('click', hideMobileMenu);
+
 // Function to toggle the visibility of the mobile menu
 function toggleMobileMenu() {
-  mobileMenu.style.display = mobileMenu.style.display === 'none' ? 'block ' : 'none';
+  mobileMenu.style.display = 'block';
+}
+
+// Function to hide the mobile menu
+function hideMobileMenu() {
+  mobileMenu.style.display = 'none';
 }
